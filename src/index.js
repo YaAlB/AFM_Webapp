abed943645c6bd444c4406eae41348d4a7f571e4
YaAlB <yassinbehery@gmail.com>
2023-10-18 13:58:42 +1100

    remove Navbar from App scope

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-// import NavBar from './components/NavBar/NavBar.js'
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App/>
-        {/* <NavBar/> */}
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
