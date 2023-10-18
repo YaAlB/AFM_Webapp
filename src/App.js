e7c6861a2834e5a368495fe184b1512a9c89f58c
YaAlB <yassinbehery@gmail.com>
2023-10-18 13:57:54 +1100

    add routes to webapp

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Public from './components/Public'
-import Login from './features/auth/Login';
-import Signup from './features/auth/Signup';
-import DashLayout from './components/DashLayout'
-import Welcome from './features/auth/Welcome'
-import ApplicationsList from './features/applications/ApplicationsList'
-import EditApplication from './features/applications/EditApplication'
-import NewApplication from './features/applications/NewApplication'
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import LoginSignup from "./features/auth/LoginSignup";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import ApplicationsList from "./features/applications/ApplicationsList";
+import EditApplication from "./features/applications/EditApplication";
+import NewApplication from "./features/applications/NewApplication";
+import Prefetch from "./features/auth/Prefetch";
+import PersistLogin from "./features/auth/PersistLogin";
+import AboutUs from "./components/AboutUs";
+import Services from "./components/Services";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="dash" element={<DashLayout />}>
 
-          <Route index element={<Welcome />} />
+        <Route path="aboutus" element={<AboutUs />} />
+        <Route path="services" element={<Services />} />
 
-          <Route path="applications">
-            <Route index element={<ApplicationsList />} />
-              <Route path=":id" element={<EditApplication />} />
-              <Route path="new" element={<NewApplication />} />
+        <Route path="auth/:value" element={<LoginSignup />} />
+
+        <Route element={<PersistLogin />}>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
+
+              <Route path="applications">
+                <Route index element={<ApplicationsList />} />
+                <Route path=":id" element={<EditApplication />} />
+                <Route path="new" element={<NewApplication />} />
+              </Route>
+            </Route>
           </Route>
-
         </Route>
-
       </Route>
     </Routes>
   );
