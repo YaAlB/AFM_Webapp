39bfcaa1da51ec5fec5e314cef8ec92540eb2a34
YaAlB <yassinbehery@gmail.com>
2023-10-18 14:56:43 +1100

    add sentery for error and performance monitoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import * as Sentry from "@sentry/react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import { store } from "./app/store";
-import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
